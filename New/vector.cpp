dc97f7d3c7c8bb9e746c57e17efc0fd2454e3a70
Towsif <towsif1528@gmail.com>
2023-08-02 15:45:23 +0600

    vector.cpp modified

--- New/vector.cpp
+++ New/vector.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-void displayVec(vector<int> tishat)
+void displayVec(vector<string> tishat)
 {
     for (int i = 0; i < tishat.size(); i++)
     {
@@ -13,13 +13,27 @@ void displayVec(vector<int> tishat)
 
 int main()
 {
-    vector<int> tishat;
+    vector<string> tishat;
+    // int opi[5] = {2, 2, 3, 4, 5};
+    int s;
+    cout << "Please enter the size of the array : ";
+    cin >> s;
+    string *opi = new string[s];
 
-    tishat.push_back(14);
-    tishat.push_back(14);
-    tishat.push_back(14);
-    tishat.push_back(14);
-    tishat.push_back(4);
+    for (int i = 0; i < s; i++)
+    {
+        cin >> opi[i];
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        tishat.push_back(opi[i]);
+    }
+    // tishat.push_back(14);
+    // tishat.push_back(14);
+    // tishat.push_back(14);
+    // tishat.push_back(14);
+    // tishat.push_back(4);
 
     displayVec(tishat);
     return 0;
